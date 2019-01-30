62e83f8103086de8163d4dd7e6871e1c333a0512
mmze <mmze@yandex.ru>
2019-01-30 16:14:15 +0300

    reload3

--- exam6.rb
+++ exam6.rb
@@ -14,131 +14,33 @@
 # WrongNumberOfPlayersError
 # rps_game_winner([%w[player1 P], %w[player2 A]])) # => NoSuchStrategyError
 # rps_game_winner([%w[player1 P], %w[player2 S]])) # => 'player2 S'
-# rps_game_winner([%w[player1 P], %w[player2 P]])) # => 'player1 P' 
-
-questarrays = [
-	[%w[player1 P], %w[player2 S]],
-	[%w[player1 P], %w[player2 P]],
-	[%w[player1 P], %w[player2 A]],
-	[%w[player1 P], %w[player2 S], %w[player3 S]]
-
-
-]
-
-b = [%w[player1 P], %w[player2 S]]
+# rps_game_winner([%w[player1 P], %w[player2 P]])) # => 'player1 P'
 
 
 # класс для ошибки WrongNumberOfPlayersError
-class WrongNumberOfPlayersError < Exception  
+class WrongNumberOfPlayersError < Exception
 end
 # класс для ошибки NoSuchStrategyError
-class NoSuchStrategyError < Exception  
-end
-# вызов своей ошибки с своим текстом
-def players(x)
-  raise WrongNumberOfPlayersError, "WrongNumberOfPlayersError #{x}"
-end
-# вызов своей ошибки с своим текстом
-def strategy
-	raise NoSuchStrategyError, "NoSuchStrategyError "
+class NoSuchStrategyError < Exception
 end
 
-def go_win(array)
-	move_p1 = array[0][1] # ход первого игрока
-	move_p2 = array[1][1] # ход второго игрока
-	p1 = array[0][0] # имя первого игрока
-	p2 = array[1][0] # имя второго игрока 
-	if move_p1 == "R" or move_p1 == "P" or move_p1 == "S" or move_p2 == "R" or move_p2 == "P" or move_p2 == "S"
-		if move_p1 == move_p2
-			puts "Ничья! Но победил игрок: "+p1+" его ход: "+move_p1
-		elsif move_p1 == "R" && move_p2 == "S" || #камень - ножницы
-			  move_p1 == "P" && move_p2 == "R" || #бумага - камень
-			  move_p1 == "S" && move_p2 == "P"	  #ножницы - бумага
-			puts " Победил первый игрок: "+p1+" его ход: "+move_p1
-		elsif move_p2 == "R" && move_p1 == "S" ||
-			  move_p2 == "P" && move_p1 == "R" ||
-			  move_p2 == "S" && move_p1 == "P" 
-			  
-			puts " Победил второй игрок: "+p2+" его ход: "+move_p2
-		else 
-			strategy()
-		end
-	else
-		strategy()
-	end
+def victory_rules?(moves)
+  return true if moves.uniq.size == 1
+  early_move = %w[R P S]
+  winner_strategy = [%w[R S], %w[P R], %w[S P], %w[S S], %w[P P], %w[R R]]
+  raise NoSuchStrategyError, "NoSuchStrategyError wrong_turn" if (early_move - moves).size > 1
+  winner_strategy.include?(moves)
 end
 
-# проверяем на количество элементов массива,
-# если != 2 то массивы сформированны не корректно
-# и сыграть по правилам не получится 
-def go(array) 
-	i = 0
-	array.each{|elem| 
-		if elem.length == 2
-			puts "все корректно"
-			i += 1
-		elsif elem.length < 2
-			puts "Нет игрока, либо нет варианта ответа"
-			return false
-		else
-			puts "что то пошло не так, кто \
-			 то из игроков использовал ещё одну руку?"
-			 return false
-		end 
-		}
-
-		if i == 2
-			return true
-		else
-			return false
-		end
+def rps_game_winner(array = [])
+  raise WrongNumberOfPlayersError, "WrongNumberOfPlayersError, number_of_players = #{array.size}" if array.size>2
+  newgame = []
+  array.each{|x| newgame<<x.last}
+  if victory_rules?(newgame)
+    array[0]
+  else
+    array[1]
+  end
 end
 
-
-
-
-def rps_game_winner(array)
-	if array.class == Array
-		if array.empty?
-			puts "Вы передали пустой массив"
-		else
-			if array.length == 2 # проверяем действительно ли переданно два массива, иначе игроков больше вызываем исключение
-				if go(array) == true
-					puts "Начинаем игру"
-					go_win(array)
-				else
-
-				end
-			elsif array.length > 2
-				players(array.length)
-			else
-				puts "Вы передали недостаточное количество игроков"
-			end	
-		end
-	else
-		puts "Вы передали не массив"
-	end
-end
-
-
-
-
-
-begin
-	questarrays.each{|elem| 
-		if elem.empty?
-			puts elem
-			puts "----------------"
-		else
-			puts rps_game_winner(elem)
-			puts "----------------"
-		end
-	}
-rescue WrongNumberOfPlayersError => e
-	puts e.backtrace
-	puts e.message
-rescue NoSuchStrategyError => e
-	puts e.backtrace
-	puts e.message
-
-end
+puts rps_game_winner([%w[player1 S], %w[player2 R]])
