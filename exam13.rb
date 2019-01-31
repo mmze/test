b447f72451a6b91cb709f829fc040754d397e6af
mmze <mmze@yandex.ru>
2019-01-31 22:30:11 +0300

    reload5

--- exam13.rb
+++ exam13.rb
@@ -1,40 +1,16 @@
 # Упражнение 13
 # Адаптируйте ваше решение из Упражнения 1 так, чтобы вместо palindrome?("foo")
 # вы могли его использовать как "foo".palindrome? # => false.
-begin
-    require 'unicode'
-    puts "gem: unicode успешно подключен"
-rescue LoadError
-    puts "!!!Не установлен gem: unicode!!!"
-    puts "---Кирилица возможно обрабатывается неверно---"
+module SayPalindrome
+		def palindrome?(str = self)
+	    return false unless str.is_a?(String) and !str.empty?
+	    ignore_str = self.gsub(/[^0-9A-Za-z]/, '').downcase
+	    ignore_str.reverse == ignore_str
+		end
 end
 
-
 class String
-
-	def palindrome?(str = self)
-	    if str == nil 
-	        return false
-	    elsif str.class == NilClass
-	        return false
-	    elsif str.empty?
-	        return true
-	    elsif str.length == 1
-	        return true
-	    else
-	    	str = self.to_s
-	        str = Unicode::downcase(str)
-	        ignore_str = str.gsub(/[^0-9A-Za-z]/, '').downcase
-	        if str.length == 0
-	            return false
-	        elsif ignore_str.reverse == ignore_str
-	            return true
-	        else
-	            return false
-	        end
-	    end
-	end
-
+	include SayPalindrome
 end
 
 puts "foo".palindrome?
