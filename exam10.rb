446561b60d60be887fa24c592b1849f9529acea2
mmze <mmze@yandex.ru>
2019-02-01 20:10:27 +0300

    private non private

--- exam10.rb
+++ exam10.rb
@@ -5,14 +5,13 @@
 # count_words("A man, a plan, a canal -- Panama")
 #  # => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
 # count_words "Doo bee doo bee doo"
-#  # => {'doo' => 3, 'bee' => 2} 
+#  # => {'doo' => 3, 'bee' => 2}
 
 def count_words(string)
-	b = string.gsub(/[,-]/, "").downcase.split(' ')
-	return b.inject(Hash.new(0)){|hash, elem| 
-	 	hash[elem] = hash[elem] + 1
-	 	hash
-	}
+  string.gsub(/[,-]/, "").downcase.split(' ').inject(Hash.new(0)) do |hash, elem|
+    hash[elem] = hash[elem] + 1
+    hash
+  end
 end
 
 puts count_words("A man, a plan, a canal -- Panama")
