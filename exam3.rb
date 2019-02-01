d15eb9c004b16478fc107e5d63717f45f6319225
mmze <mmze@yandex.ru>
2019-02-01 15:07:26 +0300

    reload6

--- exam3.rb
+++ exam3.rb
@@ -7,6 +7,7 @@
 # max_odd(['ololo', 2, 3, 4, [1, 2], nil]) # => 3
 # max_odd(%w[ololo fufufu]) # => nil
 # max_odd([2, 2, 4]) # => nil
+
 # def max_odd(array = nil)
 # 	if array.class == NilClass
 # 		return nil
@@ -32,11 +33,11 @@ questarrays = [
 	nil
 
 ]
-def max_odd(array = nil)
-	nil if array.is_a? NilClass
-	array.compact.flatten.find_all{|x| x.is_a?(Integer) or x.is_a?(Float)}.select{|x| x.to_int.odd? }.max
+def max_odd(array)
+	return nil if array.is_a? NilClass
+	a = array.compact.flatten.find_all{|x| x.is_a?(Integer) or x.is_a?(Float)}.select{|x| x.to_int.odd? }.max.to_i
+  return a if a !=0
 end
 
 # puts "Вывод тестовых заданий: "
-# questarrays.each{|elem| puts "#{elem} "+max_odd(elem).to_s}
-max_odd(nil)
+questarrays.each{|elem| puts "#{elem} "+max_odd(elem).to_s}
