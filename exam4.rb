873511b8eb7061f081acc3a332dbbbbec68f7799
mmze <mmze@yandex.ru>
2019-02-01 14:26:36 +0300

    reload6

--- exam4.rb
+++ exam4.rb
@@ -6,30 +6,29 @@
 # sort_array([2, 4, 6, 8]) # => [8, 4, 6, 2, 2]
 # sort_array([1]) # => [1, 1]
 # sort_array([1, 2, 1, 3]) # => [3, 2, 3, 1, 1]
+# array.each_with_index{|x, y| array[y] = max if x ==  min; array[y] = min if x == max}<<min
 
-
-		# x = x unless x == min or x == max
-		# x = min if x == max
-		# x = max if x == min
-	# array.each.collect{ |a| (a == min)? a = max : a }
-	# array.each.collect{ |a| (a == max)? a = min : a }
 
 questarrays = [
 	[],
     [2, 4, 6, 8],
     [1],
     [1, 2, 1, 3]
-
-
 ]
 
 def sort_array(array = [])
-		min = array.min
-    max = array.max
-    array.each_with_index{|x, y| array[y] = max if x ==  min; array[y] = min if x == max}<<min
+	min = array.min
+  max = array.max
+  array.map do |x|
+    if x == min
+      x = max
+    elsif x == max
+      x = min
+    else
+      x = x
+    end
+  end<<min
 end
-
-
 
 
 puts sort_array([1,2,1,3])
