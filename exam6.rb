c222a26b9ad7657545b1c6a99585e4ccd6d7efdb
mmze <mmze@yandex.ru>
2019-02-07 16:50:43 +0300

    branch exam6

--- exam6.rb
+++ exam6.rb
@@ -24,23 +24,27 @@ end
 class NoSuchStrategyError < Exception
 end
 
-def victory_rules?(moves)
-  return true if moves.uniq.size == 1
+def h_include?(array)
+  raise WrongNumberOfPlayersError, "WrongNumberOfPlayersError, number_of_players = #{array.size}" if array.size>2
+  a = array.to_h
+  return true if a.values.uniq.size == 1
   early_move = %w[R P S]
-  winner_strategy = [%w[R S], %w[P R], %w[S P], %w[S S], %w[P P], %w[R R]]
-  raise NoSuchStrategyError, "NoSuchStrategyError wrong_turn" if (early_move - moves.to_h.values.to_a).size > 1
-  winner_strategy.include?(moves.to_h.values)
+  winner_strategy = [%w[R S], %w[P R], %w[S P]].to_h
+  [a.values].to_h.each do |key, value|
+    puts winner_strategy[key]
+    puts key
+    puts value
+    raise NoSuchStrategyError, "NoSuchStrategyError wrong_turn" if !early_move.include?key.to_s or !early_move.include?value.to_s
+    return true if value == winner_strategy[key]
+  end
 end
 
 def rps_game_winner(array = [])
-  raise WrongNumberOfPlayersError, "WrongNumberOfPlayersError, number_of_players = #{array.size}" if array.size>2
-  if victory_rules?(array)
+  if h_include?(array)
     array[0]
   else
     array[1]
   end
 end
-puts rps_game_winner([%w[player1 S], %w[player2 R]])
 
-
-
+puts rps_game_winner([%w[player1 P], %w[player2 S]])
